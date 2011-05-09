f48d34a2a205086bb016e93cd271f0b850f91e2a
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2011-05-09 22:13:42 +0000

    Port SLHC to 4_2_X

--- interface/HcalCoderUpgrade.h
+++ interface/HcalCoderUpgrade.h
@@ -1,24 +1,25 @@
-#ifndef HCAL_CODER_DB_H
-#define HCAL_CODER_DB_H
+#ifndef HCAL_CODER_UPGRADE_H
+#define HCAL_CODER_UPGRADE_H
 
 #include "CalibFormats/HcalObjects/interface/HcalChannelCoder.h"
 #include "CalibFormats/HcalObjects/interface/QieShape.h"
 #include "CalibFormats/HcalObjects/interface/HcalCoder.h"
+#include "DataFormats/HcalDigi/interface/HcalUpgradeDataFrame.h"
 
-/** \class HcalCoderDb
+/** \class HcalCoderUpgrade
     
     coder which uses DB services to convert to fC
     $Author: ratnikov
-    $Date: 2011/01/21 22:24:37 $
-    $Revision: 1.5.6.1 $
+    $Date: 2011/01/21 22:33:39 $
+    $Revision: 1.1.2.1 $
 */
 
 class HcalQIECoder;
 class HcalQIEShape;
 
-class HcalCoderDb : public HcalCoder {
+class HcalCoderUpgrade : public HcalCoder {
 public:
-  HcalCoderDb (const HcalQIECoder& fCoder, const HcalQIEShape& fShape);
+  HcalCoderUpgrade (const HcalQIECoder& fCoder, const HcalQIEShape& fShape);
 
   virtual void adc2fC(const HBHEDataFrame& df, CaloSamples& lf) const;
   virtual void adc2fC(const HODataFrame& df, CaloSamples& lf) const;
@@ -30,10 +31,11 @@ public:
   virtual void fC2adc(const CaloSamples& clf, HODataFrame& df, int fCapIdOffset) const;
   virtual void fC2adc(const CaloSamples& clf, ZDCDataFrame& df, int fCapIdOffset) const;
   virtual void fC2adc(const CaloSamples& clf, HcalCalibDataFrame& df, int fCapIdOffset) const;
- private:
-  virtual void adc2fC(const HcalUpgradeDataFrame& df, CaloSamples& lf) const { }
-  virtual void fC2adc(const CaloSamples& clf, HcalUpgradeDataFrame& df, int fCapIdOffset) const { }
 
+  virtual void adc2fC(const HcalUpgradeDataFrame& df, CaloSamples& lf) const;
+  virtual void fC2adc(const CaloSamples& clf, HcalUpgradeDataFrame& df, int fCapIdOffset) const;
+
+ private:
   template <class Digi> void adc2fC_ (const Digi& df, CaloSamples& clf) const;
   template <class Digi> void fC2adc_ (const CaloSamples& clf, Digi& df, int fCapIdOffset) const;
 
